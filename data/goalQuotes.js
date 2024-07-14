b8a9c5cf47d4608a69548b1b6b45f2600108d54f
EzbonJoe <148608478+EzbonJoe@users.noreply.github.com>
2024-07-14 05:41:40 +0300

    Add files via upload

---
+++ data/goalQuotes.js
@@ -0,0 +1,134 @@
+export const quotes = [
+  '1.Write down your goals, make plans to achieve them, and work on your plans every single day.',
+
+  '2.You become what you think about most of the time Your outer world ultimately becomes a reflection of your inner world. Your outer world of experience mirrors back to you what you think about most of the time. Whatever you think about continuously emerges in your reality.',
+
+  '3.Happiness is the progressive realization of a worthy ideal, or goal.',
+
+  '4.It is only when you free yourself from negative emotions by accepting complete responsibility that you can begin to set and achieve goals in every area of your life. It is only when you are free, mentally and emotionally, that you canbegin to channel your energy and enthusiasm in a forward direction. Without the acceptance of complete personal responsibility, no progress is possible. On the other hand, once you accept total responsibility  for your life, there are no limits on what you can be, do, and have.',
+
+  '5.The more responsibility you accept, the greater amount of control you feel you have.',
+
+  '6.Once you accept complete responsibility for your life and for everything that happens to you and then create an ideal picture of your perfect future and clarify your values, you are ready to begin setting clear, specific goals in every area of your life. You are now on the launching ramp and ready to take off toward the stars.',
+
+  '7.The only thing that stands between a man and what he wants from life is often merely the will to try it and the faith to believe that it is possible.',
+
+  '8.When you start to walk, talk, dress, and behave like the top people, you soon will begin to feel like the top people. You will treat other people like the top people do. You will work the way the top people work. You will start to get the results that the top people get. In no time at all, you will be one of the top people yourself. It may be trite to say “Fake it until you make it!” but there is a lot of truth to it.',
+
+  '9.You will become as small as your controlling desire; as great as your dominant aspiration.',
+
+  '10.The further you think into the future, the better decisions you will make in the present to assure that that future becomes a reality.',
+
+  '11.We greatly overestimate what we can accomplish in one year. But we greatly underestimate what we can accomplish in five years.” Don’t let this happen to you. Don’t underestimate your potential.', 
+
+  '12.Realize what you really want. It stops you from chasing butterflies and puts you to work digging gold.',
+
+  '13.You have to walk before you run when setting goals.',
+
+  '14.There is one quality which one must possess to win, and that is definiteness of purpose, the knowledge of what one wants, and a burning desire to possess it.',
+
+  '15.In zero-based thinking, you ask this question: “Knowing what I now know, is there anything that I am doing today that I wouldn’t start again if I had to do it  over?”.',
+
+  '16.The true facts are what you need to make good decisions.',
+
+  '17.Those who cannot remember the past are doomed to repeat it.',
+
+  '18.Taking the time to honestly evaluate each part of your situation before you launch toward your goal will save you months and even years on your journey. In many cases, it will force you to reevaluate your goals in the light of superior analysis and knowledge. It will dramatically improve the speed at which you achieve your goals once you get going.',
+
+  '19. One of your biggest responsibilities in life, something that you and only you can take care of, is to manage your money, control your finances, and achieve financial independence in the course of your working lifetime.',
+
+  '20.You will be happy on the inside only when you are working at something on the outside that is in harmony with what is truly important to you.', 
+
+  '21. When you set off to achieve financial independence, it can seem like an overwhelming task. But when you break down your large, long-term goals into small, bite-sized pieces, the task becomes far more realistic, believable, and achievable.',
+
+  '22.The uncommon man is merely the common man thinking and dreaming of success and in more fruitful areas.',
+
+  '23.To have more, you must first be more.',
+
+  '24.How do you eat an elephant?” and the answer, “One bite at a time.” This is the same way that you become absolutely excellent at what you do. You move to the top one step, one skill, one small improvement at a time.',
+
+  '25.You begin your journey to excellence by asking the question, “What additional knowledge, skills, and information will I need to lead my field in the months and years ahead?”',
+
+  '26.The very best way to develop yourself is in the direction of your natural talents and interests. Jim Cathcart, the author and speaker, says, “Nurture your nature.” This is extremely important advice that you should follow throughout your career.',
+
+  '27.Here is the rule: You could be only one skill away from doubling your productivity, performance, and income. You may only need to bring up your skill level in one area for you to be able to use all of your other skills at a higher level.',
+
+  '28. The person who hears one idea and takes action on it is more valuable than a person who hears a hundred ideas but takes action on none of them.',
+
+  '29.Remember, anything that anyone has done, within reason, you can do as well.',
+
+  '30.The potential of the average person is like a huge ocean unsailed, a new continent unexplored, a world of possibilities waiting to be released and channeled toward some great good.',
+
+  '31 Decide what’s right before you decide what is possible. Decide what is ideal before you decide what is affordable.',
+
+  '32.The only way to increase the value and deepen the quality of your relationships is to invest more time in them. The more time you spend with a person, especially when that person is engaged in activities that are important to him or her, the deeper and richer the relationship becomes.',
+
+  '33.The weakest living creature, by concentrating his powers on a single object, can accomplish something: the strongest, by dispersing his over many, may fail to accomplish anything.',
+
+  '34.Bad habits are easy to form, but hard to live with; good habits are hard to form, but easy to live with.',
+
+  '35.There is no road too long to the man who advances deliberately and without undue haste; there are no honors too distant to the man who prepares himself for them with patience.',
+
+  '36.Goal setting enables you to use vastly more of your thinking powers than most other people.',
+
+  '37.The three keys to peak performance in achieving your goals are commitment, completion, and closure.',
+
+  '38.Much of the unhappiness that people experience is because they have not been able to discipline themselves to follow through and complete an important task or responsibility.',
+
+  '39.Perhaps the most important ability in the world of work is “depend-ability.',
+
+  '40.A goal or a decision without a deadline is merely a wish.',
+
+  '41.If You Measure It, You Can Manage It.',
+
+  '42.The person interested in success has to learn to view failure as a healthy, inevitable part of the process of getting to the top.',
+
+  '43.Failure is merely an opportunity to more intelligently begin again.',
+
+  '45.The better you get at solving problems, the faster you will be at solving each subsequent problem.',
+
+  '46.The way that you develop courage and confidence is with knowledge and skill.',
+
+  '47.Beware of any problem for which there is only one definition, or only one solution.',
+
+  '48.Your outlook upon life, your estimate of yourself, your estimate of your value are largely colored by your environment. Your whole career will be modified, shaped, molded by your surroundings, by the character of the people with whom you come in contact every day.',
+
+  '49.The more people you know, and who know you in a positive way, the more successful you will be at anything you attempt. One person, at the right time, in the right place, can open a door for you that can change your life and save you years of hard work.',
+
+  '50.Nothing will help you more in your career than to get the reputation for being the kind of person who gets the most important job done quickly and well.',
+
+  '51.The fact is that people think about themselves and their own jobs all day long. Anytime you offer to help people to do their jobs better or faster, they will be wide open to helping you later.',
+
+  '52.The most popular people in every organization are those who are always willing to lend a helping hand.',
+
+  '53.Thoroughness characterizes all successful men. Genius is the art of taking infinite pains . . . All great achievement has been characterized by extreme care, infinite painstaking, even to the minutest detail.',
+
+  '54.Action without planning is the cause of every failure.',
+
+  '55.Every minute that you spend planning and thinking before you begin will save you time, money, and energy in getting the results you desire.',
+
+  '56.There is a six P formula for personal and business success: “Proper Prior Planning Prevents Poor Performance."',
+
+  '57.There is a time for optimism and there is a time for realism in making plans and achieving goals.',
+
+  '58. Time slips through our hands like grains of sand, never to return again. Those who use time wisely are rewarded with rich, productive and satisfying lives.',
+
+  '59.The starting point of time management is for you to determine your goals and then to organize your goals by priority and value. You need to be absolutely clear, at any given moment, exactly what is most important to you at that time.At one moment, it could be a business, financial, or career goal. Later it could be a family or relationship goal. On still another occasion it could be a health or fitness goal. In each case, you must be like a sniper, zeroing in on your highest priority at the moment, rather than a machine gunner, shooting randomly by attempting to do too many things at the same time.',
+
+  '60.Thomas Edison once wrote, “My success is due more to my ability to work continuously on one thing without stopping than to any other single quality."',
+
+  '61.One rule for success is this: Develop good habits and make them your masters.',
+
+  '62.It has been said that “men and women begin to become great when they begin to listen to their inner voices.” When you make a regular habit of listening to your intuition and trusting your inner voice, you will probably never make another mistake.',
+
+  '63.Few things are impossible to diligence and skill; great works are performed not by strength but by perseverance.',
+
+  '64.The very act of gathering more information and experience in a particular area gives us more courage and confidence in that area.',
+
+  '65.The future belongs to the risk takers, not the security seekers.',
+
+  '66.Persistence Is Self-Discipline in Action.',
+
+  '67.In the later years of his life, Churchill was asked to address a class at his old preparatory school and share with the young people present what he believed to be the secret of his great success in life. He stood before the assembly, leaning on his cane, shaking a little, and said with a strong voice, “I can summarize the lessons of my life in seven words: never give in; never, never give in.”',
+
+]

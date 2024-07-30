47c22888f411339679dde792432547b188279f40
EzbonJoe <148608478+EzbonJoe@users.noreply.github.com>
2024-07-30 10:55:39 +0300

    Add files via upload

--- scripts/header.js
+++ scripts/header.js
@@ -1,5 +1,7 @@
 const dropDownEl = document.querySelector('.js-drop-down');
 const iconEl = document.querySelector('.js-icon-container');
+const displayBodyEl = document.querySelector('.display-color');
+const displayColorName = document.querySelector('.display-color-name');
 
 let isDropDownOn = false;
 
@@ -7,10 +9,14 @@ let isDropDownOn = false;
 iconEl.addEventListener('click', () => {
   if(!isDropDownOn){
     dropDownEl.style.display = "block";
+    displayBodyEl.style.display = "none";
+    displayColorName.style.display = "none";
     iconEl.innerHTML = '<img src="icons/close-icon.png" class="close-icon">';
     isDropDownOn = true;
   }else{
     dropDownEl.style.display = "none"; 
+    displayBodyEl.style.display = "initial";
+    displayColorName.style.display = "initial";
     iconEl.innerHTML = '<img src="icons/burger-menu2.png" class="icon">';
     isDropDownOn = false;
   }

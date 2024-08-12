d6c480e0966387447d8291bf6f3ee9a3cb393f9d
EzbonJoe <148608478+EzbonJoe@users.noreply.github.com>
2024-08-12 14:19:45 +0300

    Add files via upload

--- data/books.js
+++ data/books.js
@@ -1,23 +1,23 @@
 export const books = [{
   image: 'images/Mastery.jpg',
   title: 'Mastery',
-  link: 'Quotes from Mastery.html'
+  link: './Quotes from Mastery'
 },{
   image: 'images/Goals.jpg',
   title: 'Goals',
-  link: 'Quotes From Goals by Brian Tracy.html'
+  link: './Quotes From Goals by Brian Tracy'
 },{
   image: 'images/monk.jpg',
   title: 'The Monk Who Sold His Ferrari',
-  link: 'Quotes From The Monk Who Sold His Ferrari.html'
+  link: './Quotes From The Monk Who Sold His Ferrari'
 },{
   image:'images/Lead The Family Business.jpg',
   title: 'How To Lead The Family Business',
-  link: 'Quotes From How To Lead The Family Business.html'
+  link: './Quotes From How To Lead The Family Business'
 },{
   image: 'images/themillionairefastlane.jpg',
   title: 'The Millionaire Fastlane',
-  link: 'Quotes From The Millionaire Fastlane.html'
+  link: './Quotes From The Millionaire Fastlane'
 },{
   image: 'images/Way of The Superior Man.png',
   title: 'Way Of The Superior Man',
@@ -25,7 +25,7 @@ export const books = [{
 },{
   image: 'images/5am-club.jpg',
   title: 'The 5am Club',
-  link: 'Quotes from The 5am Club.html'
+  link: './Quotes from The 5am Club'
 },{
   image: 'images/Think and Grow Rich.png',
   title: 'Think And Grow Rich',
@@ -61,5 +61,5 @@ export const books = [{
 },{
   image: 'images/7 Habits Of Highly Effective People.jpg',
   title: '7 Habits Of Highly Effective People',
-  link: 'Quotes From 7 Habits of Highly Effective People.html' 
+  link: './Quotes From 7 Habits of Highly Effective People' 
 }];

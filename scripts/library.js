23fcf891f639b9bb894c6051736e9f600a11d3d5
EzbonJoe <148608478+EzbonJoe@users.noreply.github.com>
2024-09-03 07:44:13 +0300

    Add files via upload

--- scripts/library.js
+++ scripts/library.js
@@ -1,19 +1,39 @@
 import {books} from '../data/books.js';
 
-let booksHTML = '';
+function loadPage(){
+  let booksHTML = '';
 
-books.forEach((book) => {
-  booksHTML += `
-  <a href="${book.link}">
-    <div class="book-container">
-      <img class="image" src="${book.image}">
-      <div class="text-container">
-        <div class="book-title">${book.title}.</div>
+  const url = new URL(window.location.href);
+  const search = url.searchParams.get('search');
+
+  let filteredBooks = books;
+
+  if(search){
+    filteredBooks = books.filter((book) => {
+      return book.title.toLowerCase().includes(search.toLowerCase());
+    });
+  }
+  
+  filteredBooks.forEach((book) => {
+    booksHTML += `
+    <a href="${book.link}">
+      <div class="book-container">
+        <img class="image" src="${book.image}">
+        <div class="text-container">
+          <div class="book-title">${book.title}.</div>
+        </div>
       </div>
-    </div>
-  </a>`;
-  //console.log(booksHTML);
-});
+    </a>`;
+    //console.log(booksHTML);
+  });
 
-document.querySelector('.js-grid')
-  .innerHTML = booksHTML;
+  document.querySelector('.js-grid')
+    .innerHTML = booksHTML;
+
+  document.querySelector('.js-search-button').addEventListener('click', () => {
+    const search = document.querySelector('.js-search-input').value;
+    window.location.href= `Best-book-quotes-and-the-books-they-come-from.html?search=${search}`;
+  })
+}
+
+loadPage();
